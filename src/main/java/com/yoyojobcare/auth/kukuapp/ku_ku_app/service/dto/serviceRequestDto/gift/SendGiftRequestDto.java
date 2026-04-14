41811d1b33b60e85de44e54ab0f5f3432d82121f
Prince-Menaria <princemenaria04@gmail.com>
2026-04-14 10:40:53 +0530

    update logic in gift service class

--- src/main/java/com/yoyojobcare/auth/kukuapp/ku_ku_app/service/dto/serviceRequestDto/gift/SendGiftRequestDto.java
+++ src/main/java/com/yoyojobcare/auth/kukuapp/ku_ku_app/service/dto/serviceRequestDto/gift/SendGiftRequestDto.java
@@ -1,5 +1,7 @@
 package com.yoyojobcare.auth.kukuapp.ku_ku_app.service.dto.serviceRequestDto.gift;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class SendGiftRequestDto {
 
     private Long senderId;    // Means HostId or UserId
-    private Long receiverId;
+    private List<Long> receiverIds;
     private Long giftId;
     private Long roomId;
     private Integer quantity;
